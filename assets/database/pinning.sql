5afe6a8ff0e213414f236cb891eea7edf4f4cf07
Lalit Thapa <emocentmagar@gmail.com>
2015-04-04 11:58:39 +0545

    sql

--- assets/database/pinning.sql
+++ assets/database/pinning.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 01, 2015 at 11:46 पूर्वाह्न
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: Apr 04, 2015 at 08:12 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,11 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `username` text NOT NULL,
-  `password` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `password` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -47,29 +46,29 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `client` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `client` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `org` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `website` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `website` text CHARACTER SET utf8 COLLATE utf8_bin,
   `date_deal` text NOT NULL,
-  `domain_expire` text NOT NULL,
-  `hosting_expire` text NOT NULL,
+  `domain_expire` text,
+  `hosting_expire` text,
   `dealer` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `contact` text NOT NULL,
-  `email` text NOT NULL,
+  `email` text,
   `domain_status` int(11) NOT NULL DEFAULT '0',
   `hosting_status` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `dealing_price` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`id`, `client`, `org`, `website`, `date_deal`, `domain_expire`, `hosting_expire`, `dealer`, `contact`, `email`, `domain_status`, `hosting_status`) VALUES
-(1, 'asdf', 'askdfgj', 'http://www.pinesofts.com', '2015-03-07', '2015-03-29', '2015-03-19', '1', '9887867', 'prachanda.gurung@gmail.com', 1, 1),
-(2, 'himalayan tiger', 'trek and adventure', 'http://www.htnpl.com', '2015-03-17', '2015-03-30', '2015-03-31', '2', '8796543', 'czinka62@yahoo.com', 1, 1),
-(3, 'the ', 'jukson.com', 'http://www.chartjs.org/docs/', '2015-01-01', '2015-01-01', '2015-01-31', '2', '878566545', 'czinka62@yahoo.com', 0, 0);
+INSERT INTO `client` (`id`, `client`, `org`, `website`, `date_deal`, `domain_expire`, `hosting_expire`, `dealer`, `contact`, `email`, `domain_status`, `hosting_status`, `dealing_price`) VALUES
+(1, 'asdf', 'askdfgj', 'http://www.pinesofts.com', '2015-03-07', '2015-03-29', '2015-03-19', '1', '9887867', 'prachanda.gurung@gmail.com', 1, 1, 0),
+(2, 'himalayan tiger askdjf', 'trek and adventure', 'http://www.htnpl.com', '2015-03-17', '2015-03-30', '2015-03-31', '2', '8796543', 'czinka62@yahoo.com', 1, 1, 0),
+(3, 'the jukson', 'jukson.com', 'http://www.chartjs.org/docs/', '2015-01-01', '2015-01-01', '2015-01-31', '2', '878566545', 'czinka62@yahoo.com', 0, 0, 20000);
 
 -- --------------------------------------------------------
 
@@ -78,14 +77,13 @@ INSERT INTO `client` (`id`, `client`, `org`, `website`, `date_deal`, `domain_exp
 --
 
 CREATE TABLE IF NOT EXISTS `expense` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `expense` text NOT NULL,
   `amount` int(11) NOT NULL,
   `date` text NOT NULL,
   `year` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `month` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `month` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `expense`
@@ -95,7 +93,8 @@ INSERT INTO `expense` (`id`, `expense`, `amount`, `date`, `year`, `month`) VALUE
 (4, 'additionla', 10000, '2015-12-31', '2015', '12'),
 (5, 'tiffin', 2000, '2016-01-30', '2016', '01'),
 (6, 'internet', 12344, '2015-03-28', '2015', '03'),
-(7, 'tiffin', 12345, '2015-12-02', '2015', '12');
+(7, 'tiffin', 12345, '2015-12-02', '2015', '12'),
+(8, 'tiffin', 123, '2015-12-01', '2015', '12');
 
 -- --------------------------------------------------------
 
@@ -104,14 +103,13 @@ INSERT INTO `expense` (`id`, `expense`, `amount`, `date`, `year`, `month`) VALUE
 --
 
 CREATE TABLE IF NOT EXISTS `funding` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `taken_by` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `amount` int(11) NOT NULL,
   `date` text NOT NULL,
   `year` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `month` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `month` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `funding`
@@ -127,24 +125,20 @@ INSERT INTO `funding` (`id`, `taken_by`, `amount`, `date`, `year`, `month`) VALU
 --
 
 CREATE TABLE IF NOT EXISTS `income` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
-  `dealed_price` int(11) NOT NULL,
-  `dealed_on` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `year` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `month` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `advance_amount` int(11) NOT NULL,
+  `received_date` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `received_by` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `due_amount` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `received_amount` int(11) NOT NULL,
+  `remarks` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `income`
 --
 
-INSERT INTO `income` (`id`, `client_id`, `dealed_price`, `dealed_on`, `year`, `month`, `advance_amount`, `received_by`, `due_amount`) VALUES
-(6, 3, 25000, '2014-12-21', '2014', '12', 15000, '1', 10000);
+INSERT INTO `income` (`id`, `client_id`, `received_date`, `received_by`, `received_amount`, `remarks`) VALUES
+(6, 3, '2014-12-21', '1', 25000, '');
 
 -- --------------------------------------------------------
 
@@ -153,15 +147,14 @@ INSERT INTO `income` (`id`, `client_id`, `dealed_price`, `dealed_on`, `year`, `m
 --
 
 CREATE TABLE IF NOT EXISTS `project_logs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `date` text NOT NULL,
   `start_time` text NOT NULL,
   `end_time` text NOT NULL,
   `commitment` text NOT NULL,
   `user_id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `project_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `project_logs`
@@ -173,7 +166,11 @@ INSERT INTO `project_logs` (`id`, `date`, `start_time`, `end_time`, `commitment`
 (3, '2015-01-02', '01:00', '01:00', 'hey this is commitment', 2, 1),
 (4, '2016-12-01', '01:58', '02:00', 'hard one', 2, 1),
 (5, '2015-01-01', '01:01', '13:59', 'asdfj jakshdf kasdhf  kjshdf a', 2, 1),
-(6, '2015-12-31', '12:59', '00:00', 'lorem epsum is te aasfn ', 2, 1);
+(6, '2015-12-31', '12:59', '00:00', 'lorem epsum is te aasfn ', 2, 1),
+(7, '2015-12-01', '01:00', '01:00', 'laksdf', 2, 2),
+(8, '2015-01-01', '01:00', '01:00', 'lkadnsf', 2, 2),
+(9, '2015-04-04', '08:07', '08:09', 'Playing with the project.', 2, 1),
+(10, '2015-09-09', '09:07', '15:04', 'askdn klasdn f', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -182,12 +179,11 @@ INSERT INTO `project_logs` (`id`, `date`, `start_time`, `end_time`, `commitment`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
-  `email` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `email` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -197,6 +193,91 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
 (1, 'pracanda', 'd3d3c9b08aa454d3d3512fd20bd686e65f7f75d2', 'prachanda.gurung@gmail.com'),
 (2, 'pine', '70f910cbd2983be954d35a35bb8c6ce6d79de7ce', 'czinka62@hotmail.com');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expense`
+--
+ALTER TABLE `expense`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `funding`
+--
+ALTER TABLE `funding`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `income`
+--
+ALTER TABLE `income`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project_logs`
+--
+ALTER TABLE `project_logs`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `expense`
+--
+ALTER TABLE `expense`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `funding`
+--
+ALTER TABLE `funding`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `income`
+--
+ALTER TABLE `income`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `project_logs`
+--
+ALTER TABLE `project_logs`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
